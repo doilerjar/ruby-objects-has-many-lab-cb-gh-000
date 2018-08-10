978470d7d1176699d0d6654c99c94725508abd1c
Kevin K <doilerjar@outlook.com>
2018-08-10 22:17:06 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,5 +1,7 @@
 class Author
   
+  @@post_count = 0
+  
   attr_accessor :name
   
   def initialize(name)
@@ -7,8 +9,18 @@ class Author
   end 
   
   
-  def add_post(post)
-    
+  def add_post(a_post)
+    a_post.author = self
+    @posts << a_post
   end 
   
+  def add_post_by_title(title)
+    a_post = Post.new(title)
+    a_post.author = self
+    @posts << a_post
+  end
+  
+  def self.post_count
+    @@post_count
+  end
 end 
