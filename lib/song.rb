b01e09973fe97c135f434362f8203a9303c3770d
Kevin K <doilerjar@outlook.com>
2018-08-10 21:35:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,15 @@
 class Song
+  @@all_songs = []
   
   attr_accessor :name, :artist
   
   def initialize(name)
     @song = name
+    @@all_songs += 1
+  end
+  
+  def song_count
+    @all_songs.length
   end
   
 end 
