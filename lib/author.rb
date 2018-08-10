d1f0c96af09f6882d72cb4dd0999819c5a817216
Kevin K <doilerjar@outlook.com>
2018-08-10 22:37:51 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -13,12 +13,14 @@ class Author
   def add_post(a_post)
     a_post.author = self
     @posts << a_post
+    @@post_count += 1
   end 
   
   def add_post_by_title(title)
     a_post = Post.new(title)
     a_post.author = self
     @posts << a_post
+    @@post_count += 1
   end
   
   def self.post_count
